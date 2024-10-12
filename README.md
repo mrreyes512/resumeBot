67273d2eb966bd9cb622fb202cab3d363af5af92
Mark Reyes <mrreyes512@gmail.com>
2024-10-12 18:49:28 -0500

    checkingin

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Resume, with AI
+This will be my resume with a webex bot and AI
+
+## References
+
+- YouTube: [Working with Adaptive Cards](https://youtu.be/q4LaBvMePTw?si=T1dUK0koqfx7esvy)
---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.webex_bot import WebexBot
+
+# (Optional) Proxy configuration
+# Supports https or wss proxy, wss prioritized. 
+proxies = {
+    'https': 'http://proxy.esl.example.com:80',
+    'wss': 'socks5://proxy.esl.example.com:1080'
+}
+
+# Create a Bot Object
+bot = WebexBot(teams_bot_token=os.getenv("WEBEX_TEAMS_ACCESS_TOKEN"),
+               bot_name="reyBot",
+               include_demo_commands=True
+               )
+
+# Add new commands for the bot to listen out for.
+bot.add_command(EchoCommand())
+
+# Call `run` for the bot to wait for incoming messages.
+bot.run()
